8251d75473971cfed13bfbf2a0a409e13888e60d
Sam Hall <mann.samantha11@gmail.com>
2021-02-09 11:43:32 -0500

    POST Deliverable

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ const ramenImage = document.querySelector(".detail-image")
 const ramenName = document.querySelector(".name")
 const ramenRestaurant = document.querySelector(".restaurant")
 const ramenRating = document.querySelector("#rating")
+const newRamenForm = document.querySelector("#new-ramen")
 const ramenComment = document.querySelector("#comment")
+const deleteButton = document.querySelector("#delete")
+
 const ramenUrl = "http://localhost:3000/ramens"
 
 fetch(ramenUrl)
@@ -23,12 +26,11 @@ function makeMenuItem(dish){
     menu.append(dishImage)
 }
 
-function fetchItem(event){
-    if (event.target.className === "dish"){
-        fetch(ramenUrl + `/${event.target.dataset.id}`)
+function fetchItem(id = 1){
+    // const itemId = event.target.dataset.id || 1
+        fetch(ramenUrl + `/${id}`)
         .then(response => response.json())
-        .then(dishData => showItem(dishData))
-    }
+        .then(dishData => showItem(dishData)) 
 }
 
 function showItem(dishData){
@@ -40,6 +42,7 @@ function showItem(dishData){
     ramenRating.value = dishData.rating           
     ramenComment.value = dishData.comment
     ratingForm.dataset.id = dishData.id        
+    deleteButton.dataset.id = dishData.id
 }   
 
 function patchItem(event){
@@ -56,7 +59,52 @@ function patchItem(event){
     // .then(dishData => showItem(dishData))
 }
 
+function postItem(event){
+    event.preventDefault()
+    let newRamen = {
+        name: event.target.name.value,
+        restaurant: event.target.restaurant.value,
+        image: event.target.image.value,
+        rating: event.target.rating.value,
+        comment: event.target[4].value
+    }
+    let configObject = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newRamen)
+    }
+    fetch(ramenUrl, configObject)
+    .then(response => response.json())
+    .then(newItem => createNewItem(newItem))
+    event.target.reset()
+}
+
+function createNewItem(newItem){
+    makeMenuItem(newItem)
+    showItem(newItem)
+}
+
+function deleteItem(event){
+    let itemId = event.target.dataset.id
+    let nextId = itemId == 1 ? itemId +1 : itemId -1
+    let configObject = {
+        method: "DELETE"
+    }
+    // fetch(ramenUrl + `/${itemId}`, configObject)
+    // .then(fetchItem(nextId))
+
+    Array.from(menu.children).find(dish => dish.dataset.id == itemId).remove()
+}
 
 
-menu.addEventListener("click", fetchItem)
+
+fetchItem()
+menu.addEventListener("click", event => {
+    if (event.target.className === "dish"){
+        fetchItem(event.target.dataset.id)
+    }
+})
+
 ratingForm.addEventListener("submit", patchItem)
+newRamenForm.addEventListener("submit", postItem)
+deleteButton.addEventListener("click", deleteItem)
